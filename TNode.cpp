05b3477bccebb1f3ffd32dcc4936aff72aaae1ad
Kathleen Higgins <kathigg@udel.edu>
2025-10-19 19:27:51 -0400

    completed print for tnode

--- TNode.cpp
+++ TNode.cpp
@@ -23,3 +23,22 @@ TNode::TNode(const string sp, const string st, const string inf) {
     animal = new Species(sp, st, inf);
     height = 1;
 }
+
+/* DESTRUCTION TIME!!! */
+TNode::~TNode() {
+    delete animal;
+}
+
+/* print node info */
+void TNode::printNode(const bool XtraInfo) {
+    std::cout << "\t" << animal->name << std::endl;
+    std::cout << "\t" << animal->status << std::endl;
+    std::cout << "\t" << animal->info << std::endl;
+    if (XtraInfo) {
+        std::cout << "\tHeight: " << height << std::endl;
+        std::cout << "\tLeft Child: " << (left ? left->animal->name : "NULL") << std::endl;
+        std::cout << "\tRight Child " << (right ? right->animal->name : "NULL") << std::endl;
+    }
+}
+
+
